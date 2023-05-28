7d50d789df7d06523263c718159416589ca0b3dc
JJH <jakebbak@gmail.com>
2023-05-28 21:13:11 +0900

    2023/05/28: IP와 Port를 저장하는 클래스

---
+++ src/main/java/SpringMVC2/TypeConverter/type/IpPort.java
@@ -0,0 +1,17 @@
+package SpringMVC2.TypeConverter.type;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@EqualsAndHashCode //모든 필드를 사용하여 equals(), hashcode() 생성 ==> IpPort 객체로 a.equals(b) 가능
+public class IpPort {
+
+    private String ip;
+    private int port;
+
+    public IpPort(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    }
+}
