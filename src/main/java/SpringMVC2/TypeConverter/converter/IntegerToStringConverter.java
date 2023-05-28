b2947130137d2e0fd66f6ea4dcac643b996b8281
JJH <jakebbak@gmail.com>
2023-05-28 21:13:42 +0900

    2023/05/28: Integer --> String 변환 클래스

---
+++ src/main/java/SpringMVC2/TypeConverter/converter/IntegerToStringConverter.java
@@ -0,0 +1,15 @@
+package SpringMVC2.TypeConverter.converter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.convert.converter.Converter;
+
+//String --> Integer Converver
+@Slf4j
+public class IntegerToStringConverter implements Converter<Integer, String> {
+
+    @Override
+    public String convert(Integer s) {
+        log.info("convert source={}", s);
+        return String.valueOf(s);
+    }
+}
