de2001dabe98a9a946d2a3d89955a65b31918d35
JJH <jakebbak@gmail.com>
2023-05-29 19:50:14 +0900

    2023/05/29: 사용자 정의 Formatter - Number

---
+++ src/test/java/SpringMVC2/TypeConverter/formatter/NumberFormatterTest.java
@@ -0,0 +1,34 @@
+package SpringMVC2.TypeConverter.formatter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Locale;
+
+class NumberFormatterTest {
+
+    NumberFormatter formatter = new NumberFormatter();
+
+
+    @Test
+    public void parse() throws Exception {
+        //given
+        Number result = formatter.parse("1,000", Locale.KOREA);
+
+        //when
+
+        //then
+        Assertions.assertThat(result).isEqualTo(1000L);
+    }
+
+    @Test
+    public void print() throws Exception {
+        //given
+        String result = formatter.print(1000, Locale.KOREA);
+
+        //when
+
+        //then
+        Assertions.assertThat(result).isEqualTo("1,000");
+    }
+}
