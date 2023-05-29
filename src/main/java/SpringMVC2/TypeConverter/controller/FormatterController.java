cef659afb13fe49500b271cfccc269e54d676400
JJH <jakebbak@gmail.com>
2023-05-29 19:53:53 +0900

    2023/05/29: Formatter 컨트롤러

---
+++ src/main/java/SpringMVC2/TypeConverter/controller/FormatterController.java
@@ -0,0 +1,48 @@
+package SpringMVC2.TypeConverter.controller;
+
+
+import lombok.Data;
+import org.springframework.cglib.core.Local;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDateTime;
+
+
+/*
+[스프링 제공 기본 포메터]
+1. @NumberFormat : 숫자 형식 지정
+2. @DateTimeFormat : 날짜 형식 지정
+ */
+@Controller
+public class FormatterController {
+
+    @GetMapping("/formatter/edit")
+    public String formatterForm(Model model) {
+        Form form = new Form();
+        form.setNumber(10000);
+        form.setLocalDateTime(LocalDateTime.now());
+        model.addAttribute("form", form);
+
+        return "formatter-form";
+    }
+
+    @PostMapping("/formatter/edit")
+    public String formatterEdit(@ModelAttribute Form form) {
+        return "formatter-view";
+    }
+
+    @Data
+    static class Form {
+        @NumberFormat(pattern = "###,###")
+        private Integer number;
+
+        @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime localDateTime;
+    }
+}
